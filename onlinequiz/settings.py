3dcc7ba0f858b4c73426d4f76fa9c4acb6135488
Sujeetdeore777 <dsujeet775@gmail.com>
2023-02-13 10:28:41 +0530

    update

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '@k0#p3kidu)yaaa3u1hplxz)f@^6xiy384*(+n@@s5x#1bx@m5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["http://127.0.0.1:8000/","https://quizapp.sujeetdeore777.repl.co"]
 
 
 # Application definition
